dab96be5f1a11f531c392bca6ac087484bcf34a9
Vladyslav Lopatin <vladyslav.lopatin@wskz.pl>
2023-04-27 09:33:37 +0200

    fixed style

--- resources/views/components/breadcrumbs.blade.php
+++ resources/views/components/breadcrumbs.blade.php
@@ -4,7 +4,7 @@
             @foreach($breadcrumbs as $breadcrumb)
                 @isset($breadcrumb['route'])
                     <li>
-                        <a href="/{{ $breadcrumb['route'] }}" class="text-blue-600 hover:text-blue-700 flex space-x-1 items-center">
+                        <a href="/{{ $breadcrumb['route'] }}" class="text-link flex space-x-1 items-center">
                             @if($loop->first)
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
